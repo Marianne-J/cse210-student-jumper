f1cacd7cd89840fa04ca788effe0c6bd12868af8
Noctumbras <stephen64s@byui.edu>
2021-10-14 20:50:20 -0500

    added comments to get_current_jumper, added newlines to the string

--- jumper/game/jumper.py
+++ jumper/game/jumper.py
@@ -8,8 +8,16 @@ class Jumper:
         self.mistakes += 1
 
     def get_current_jumper(self):
+        """Using current_jumper and a for loop, puts together a string to return to Director.
+        The string is used to show the current state of the jumper.
+
+        Args:
+            self (Jumper): An instance of Jumper.
+        Returns:
+            string (visual): The current visual state of the jumper.
+        """
         visual = ""
         for x in range(self.mistakes, len(self.current_jumper)):
-            visual += self.current_jumper[x]
+            visual += self.current_jumper[x] + "\n"
 
         return visual
