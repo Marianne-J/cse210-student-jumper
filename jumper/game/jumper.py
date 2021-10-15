420bdce1e97c3b7fd42b971f91d82c5a0655631c
Kenyon <jon20019@byui.edu>
2021-10-14 18:30:07 -0600

    1

--- jumper/game/jumper.py
+++ jumper/game/jumper.py
@@ -2,4 +2,19 @@
 
 class Jumper:
 
-  pass
+    def cut_line(self):
+        self.mistakes += 1
+
+        return self.mistakes
+
+    def current_jumper(self):
+        current_jumper = ['  ___  ', ' /___\\ ', ' \\   / ', '  \\ /  ', '   0   ', '  /I\\  ', '  / \\  ', '', '^^^^^^^']
+
+        return current_jumper
+
+    def get_current_jumper(self):
+        visual = ""
+        for x in range(self.mistakes, len(self.current_jumper)):
+            visual += self.current_jumper[x]
+
+        return visual
